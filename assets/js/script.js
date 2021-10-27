6d49215cf2532133114fbdd29ca2d8f017551c0e
Ryan Oliveira <oryan424@gmail.com>
2021-10-27 20:52:00 -0300

    Consuming the  API

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+document.getElementById('btn-search').addEventListener('click', () => {
+    const search = document.getElementById('input-number');
+
+    const image = document.getElementById('image');
+    const name = document.getElementById('name');
+    const gender = document.getElementById('gender');
+    const origin = document.getElementById('origin');
+
+    if (!search.value || search.value < 1 || search.value > 671) {
+        search.style.boxShadow = '0 0 0.5px 2px #F24D4D';
+        search.style.caretColor = '#F24D4D'
+        search.focus();
+
+        return;
+    }
+
+    fetch(`https://rickandmortyapi.com/api/character/${search.value}`)
+    .then(response => response.json())
+    .then(json => {
+        image.setAttribute('src', json.image);
+        name.innerHTML = json.name;
+        gender.innerHTML = json.gender;
+        origin.innerHTML = json.origin.name;
+
+        search.value = '';
+        search.style.caretColor = '#062931'
+        search.style.boxShadow = '';
+    });
+});
